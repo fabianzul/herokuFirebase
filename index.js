890077aa1da9af006565a0af4f14defc2edba1cf
carlos <admin@lumindtech.com>
2017-09-29 03:54:45 -0500

    Ensayando con Firebase Envio de mensajes

--- index.js
+++ index.js
@@ -148,6 +148,11 @@ app.get("/miner-alert/:miner/:alert", function(request,response){
 	    //console.log(snapshot.val());
 	    var key = snapshot.key;
 		console.log(key);
+		var refAlerts = db.ref("miners/"+key+"/alerts/").push();
+
+		refAlerts.set({
+			alerts: alert
+		});
 	    //console.log(snapshot.val().propietario);
 	    //console.log(snapshot.val().tokens[0]);
 	    snapshot.val().tokens.forEach(function(token){
